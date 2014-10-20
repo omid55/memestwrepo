521b92b28b0d86a52d2015ae8c063b543fe6504a
omid55 <omidas55@gmail.com>
2014-10-20 20:11:52 +0330

    okk

--- workspace/MemetrackerTwitterProject/plotccdf_memes_twitter.cpp
+++ workspace/MemetrackerTwitterProject/plotccdf_memes_twitter.cpp
@@ -33,8 +33,8 @@ void myplotSimpleCCDF(double* arr1, int leng1, double* arr2, int leng2, char* le
 	TStr name = TStr::Fmt("CCDF_%s_%s",legend1,legend2);
 	printf("%s Drawing:\n",name.CStr());
 	plot.SetXYLabel(xlabel, "P(X>d)");
-	plot.AddPlot(points1,gpwLines,legend1);
-//	plot.AddPlot(points2,gpwLines,TStr::Fmt("%s on Twitter",legend2));
+//	plot.AddPlot(points1,gpwLines,legend1);
+	plot.AddPlot(points2,gpwLines,TStr::Fmt("%s on Twitter",legend2));
 	plot.AddCmd("set terminal postscript enhanced eps 30 color");
 	plot.SetDataPlotFNm(TStr::Fmt("MyResults/%s.tab",name.CStr()), TStr::Fmt("MyResults/%s.plt",name.CStr()));
 	plot.SaveEps(TStr::Fmt("MyResults/%s.eps",name.CStr()));
